6a55d97ae35167dd88d3ad4b1a969090385b9de0
LandonTopy <landontopy@gmail.com>
2023-10-10 09:13:06 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double gallons = 0;
+        double mpg = 0;
+        double price = 0;
+        double cost100;
+        double distance;
+        boolean done1 = false;
+        boolean done2 = false;
+        boolean done3 = false;
+
+
+        do{
+            System.out.println("Enter Current Number of Gallons of Gasoline in Vehicle:");
+            if (scan.hasNextDouble()) {
+                gallons = scan.nextDouble();
+                scan.next();
+                if (gallons >= 0){
+                    done1 = true;
+                } else {
+                    System.out.println("The value you entered was incorrect. Please try again.");
+                }
+            } else {
+                System.out.println("The value you entered was incorrect. Please try again.");
+                scan.next();
+            }
+        } while(!done1);
+
+        do{
+            System.out.println("Enter Fuel Efficiency in MPG:");
+            if (scan.hasNextDouble()) {
+                mpg = scan.nextDouble();
+                scan.next();
+                if (mpg > 0){
+                    done2 = true;
+                } else {
+                    System.out.println("The value you entered was incorrect. Please try again.");
+                }
+            } else {
+                System.out.println("The value you entered was incorrect. Please try again.");
+                scan.next();
+            }
+        } while(!done2);
+        do{
+
+            System.out.println("Enter Current Price for Gallon of Gas:");
+            if (scan.hasNextDouble()) {
+                price = scan.nextDouble();
+                scan.next();
+                if (price > 0){
+                    done3 = true;
+                } else {
+                    System.out.println("The value you entered was incorrect. Please try again.");
+                }
+            } else {
+                System.out.println("The value you entered was incorrect. Please try again.");
+                scan.next();
+            }
+        } while(!done3);
+         cost100 = (100/mpg) * price;
+         distance = gallons * mpg;
+        System.out.printf("Cost per 100 miles in $: %-8.2f", cost100);
+        System.out.printf("\nDistance left in Miles : %-8.2f", distance);
     }
 }
